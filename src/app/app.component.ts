6e8ee2e27742b230fd0b56c270cea454aafd3666
Edvin Galoyan <edvin.galoyan@synisys.com>
2021-11-09 10:45:18 +0400

    using fetch to add users in view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'angular-first-projet';
+  foo: Array<any> | undefined;
+
+  ngOnInit(): void {
+    fetch("https://jsonplaceholder.typicode.com/posts").then(result => {
+      result.json().then(res => {
+        this.foo = res;
+      })
+    })
+  }
 }
